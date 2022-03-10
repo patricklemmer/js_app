4360c6dbccbc4e3bf69ef83f51e1889bf7563d09
meerkatstudio <patrick.lemmer@yahoo.com>
2022-03-10 19:38:48 +0100

    added a title to index.html

---
+++ js/scripts.js
@@ -0,0 +1,21 @@
+let myName = 'Bob';
+document.write(myName);
+
+myName = 'John Doe';
+document.write(myName);
+
+let myAge = 34 * 4;
+document.write (myAge);
+
+let size = 100;
+let doubleSize = size * 2;
+let minSize = (doubleSize * 2) - (size / 2);
+document.write(minSize);
+
+alert('Hello World');
+
+let favouriteFood = 'Sushi';
+document.write (favouriteFood);
+
+let bestPlaceOnEarth = 'African wilderness';
+document.write(bestPlaceOnEarth);
