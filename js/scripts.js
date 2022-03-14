9ccbfae4cc1dd00bfb929df5f8724655f3419ddc
meerkatstudio <patrick.lemmer@yahoo.com>
2022-03-14 16:09:43 +0100

    added data to variable pokemonList in scripts.js

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+let pokemonList = [
+    {name: 'Charizard', height: 5, type: ['fire', 'flying']},
+    {name: 'Noctowl', height: 5, type: ['normal', 'flying']},
+    {name: 'Tauros', height: 4, type: 'normal'},
+    {name: 'Torterra', height: 7, type: ['grass', 'ground']},
+    {name: 'Squirtle', height: 1, type: 'water'},
+    {name: 'Jolteon', height: 2, type: 'electric'}
+];
