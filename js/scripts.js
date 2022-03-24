f373c2b87da92182e2dc9bb743d84e05b633fa13
meerkatstudio <patrick.lemmer@yahoo.com>
2022-03-24 10:43:20 +0100

    wrapped pokemonList in IIFE and returned list

--- js/scripts.js
+++ js/scripts.js
@@ -1,49 +1,71 @@
-let pokemonList = [
-    {
-        name: 'Charizard',
-        height: 5, weight: 199,
-        category: 'flame',
-        type: ['fire', 'flying']
-    },
-    {
-        name: 'Noctowl',
-        height: 5,
-        weight: 89,
-        category: 'owl',
-        type: ['normal', 'flying']
-    },
-    {
-        name: 'Tauros',
-        height: 4,
-        weight: 194,
-        category: 'wild bull',
-        type: ['normal']
-    },
-    {
-        name: 'Torterra',
-        height: 7,
-        weight: 683,
-        category: 'continent',
-        type: ['grass', 'ground']
-    },
-    {
-        name: 'Squirtle',
-        height: 1,
-        weight: 19,
-        category: 'tiny turtle',
-        type: ['water']
-    },
-    {
-        name: 'Jolteon',
-        height: 2,
-        weight: 54,
-        category: 'lightning',
-        type: ['electric']
-    }];
+let pokemonRepository = (function () {
+    let pokemonList = [
+        {
+            name: 'Charizard',
+            height: 5, weight: 199,
+            category: 'flame',
+            type: ['fire', 'flying']
+        },
+        {
+            name: 'Noctowl',
+            height: 5,
+            weight: 89,
+            category: 'owl',
+            type: ['normal', 'flying']
+        },
+        {
+            name: 'Tauros',
+            height: 4,
+            weight: 194,
+            category: 'wild bull',
+            type: ['normal']
+        },
+        {
+            name: 'Torterra',
+            height: 7,
+            weight: 683,
+            category: 'continent',
+            type: ['grass', 'ground']
+        },
+        {
+            name: 'Squirtle',
+            height: 1,
+            weight: 19,
+            category: 'tiny turtle',
+            type: ['water']
+        },
+        {
+            name: 'Jolteon',
+            height: 2,
+            weight: 54,
+            category: 'lightning',
+            type: ['electric']
+        }
+    ]
+
+    function add(pokemon) {
+        pokemonList.push(pokemon);
+    }
+
+    function getAll() {
+        return pokemonList;
+    }
+
+    return {
+        add: add,
+        getAll: getAll,
+        pokemonList: pokemonList
+    };
+
+})();
+
+console.log(pokemonRepository.getAll());
+pokemonRepository.add({ name: 'Ursaring', height: 5, weight: 277, category: 'hibernator', type: ['normal'] });
+console.log(pokemonRepository.getAll());
 
 // forEach function to iterate over Pokemon in pokemonList array
 function pokemonFunction(pokemon) {
     document.write("<p>" + pokemon.name + ' is ' + pokemon.height + ' ft tall.' + "</p>");
 }
 
-pokemonList.forEach(pokemonFunction);
+pokemonRepository.pokemonList.forEach(pokemonFunction);
