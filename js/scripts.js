7c6325c09d4e483496eb0cea5b176e98a00e19a4
meerkatstudio <patrick.lemmer@yahoo.com>
2022-04-05 00:49:05 +0200

    added function to fetch data from API

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,7 @@
 let pokemonRepository = (function () {
     let pokemonList = [];
+    let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
+
     // Public functions
     function add(pokemon) {
         pokemonList.push(pokemon);
@@ -32,23 +34,46 @@ let pokemonRepository = (function () {
         addListener(button, pokemon);
     }
 
+    function loadList() {
+        return fetch(apiUrl).then(function (response) {
+            return response.json();
+        }).then(function (json) {
+            json.results.forEach(function (item) {
+                let pokemon = {
+                    name: item.name,
+                    detailsUrl: item.url
+                };
+                add(pokemon);
+            });
+        }).catch(function (e) {
+            console.error(e);
+        })
+    }
+
+
     return {
         add: add,
         getAll: getAll,
-        pokemonList: pokemonList,
-        addListItem: addListItem,
-        showDetails: showDetails
+        // pokemonList: pokemonList,
+        // addListItem: addListItem,
+        // showDetails: showDetails
+        loadList: loadList
     };
-
 })();
 
-console.log(pokemonRepository.getAll());
-
-// Adds a Pokemon
-pokemonRepository.add({ name: 'Ursaring', height: 5, weight: 277, category: 'hibernator', type: ['normal'] });
-console.log(pokemonRepository.getAll());
-
-// forEach function to iterate over Pokemon in pokemonList array
-pokemonRepository.getAll().forEach(function (pokemon) {
-    pokemonRepository.addListItem(pokemon)
+pokemonRepository.loadList().then(function () {
+    pokemonRepository.getAll(forEach(function (pokemon) {
+        pokemonRepository.addListItem(pokemon);
+    }));
 });
+
+// console.log(pokemonRepository.getAll());
+
+// // Adds a Pokemon
+// pokemonRepository.add({ name: 'Ursaring', height: 5, weight: 277, category: 'hibernator', type: ['normal'] });
+// console.log(pokemonRepository.getAll());
+
+// // forEach function to iterate over Pokemon in pokemonList array
+// pokemonRepository.getAll().forEach(function (pokemon) {
+//     pokemonRepository.addListItem(pokemon)
+// });
