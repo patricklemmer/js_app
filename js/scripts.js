bf5631845defd46994f0bbfeab451bb86e5269c8
meerkatstudio <patrick.lemmer@yahoo.com>
2022-04-05 00:24:52 +0200

    cleared pokemonList array

--- js/scripts.js
+++ js/scripts.js
@@ -1,47 +1,5 @@
 let pokemonRepository = (function () {
-    let pokemonList = [
-        {
-            name: 'Charizard',
-            height: 5, weight: 199,
-            category: 'flame',
-            type: ['fire', 'flying']
-        },
-        {
-            name: 'Noctowl',
-            height: 5,
-            weight: 89,
-            category: 'owl',
-            type: ['normal', 'flying']
-        },
-        {
-            name: 'Tauros',
-            height: 4,
-            weight: 194,
-            category: 'wild bull',
-            type: ['normal']
-        },
-        {
-            name: 'Torterra',
-            height: 7,
-            weight: 683,
-            category: 'continent',
-            type: ['grass', 'ground']
-        },
-        {
-            name: 'Squirtle',
-            height: 1,
-            weight: 19,
-            category: 'tiny turtle',
-            type: ['water']
-        },
-        {
-            name: 'Jolteon',
-            height: 2,
-            weight: 54,
-            category: 'lightning',
-            type: ['electric']
-        }
-    ]
+    let pokemonList = [];
     // Public functions
     function add(pokemon) {
         pokemonList.push(pokemon);
